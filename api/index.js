4c1f29ae9622802f62dcfa6d8f7c0b3c62a57e43
NO ONE <sohaibikram249@gmail.com>
2024-09-19 18:06:41 +0500

    update

--- api/index.js
+++ api/index.js
@@ -10,24 +10,10 @@ const connect = require("../utils/db");
 const errorMiddleware = require("../middlewares/error-middleware");
 
 const Corsoptions = {
-  origin: "https://frontend-solo-tech.vercel.app", // Remove the trailing slash and ensure it's correct
+  origin: ["https://frontend-solo-tech.vercel.app"],
   methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
   credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"], // Add any additional headers you need
 };
-
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://frontend-solo-tech.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200); // Preflight response for OPTIONS requests
-  }
-  next();
-});
-
 
 app.use(cors(Corsoptions));
 app.use(express.json());
